1c717fbc82ae6ada356a0be3404f69da3efa2162
raulmarkuskivisa <raul-markus.kivisalu@khk.ee>
2018-12-07 10:41:05 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544776579;s:7:"authKey";s:64:"BFP.d)/zG+G~aU(gV0>_rA3K%3|.(V7GA!.FXoV9r#[nyBPL!:!y&*$ut82t,JIM";}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1544776579;s:7:"authKey";s:64:"BFP.d)/zG+G~aU(gV0>_rA3K%3|.(V7GA!.FXoV9r#[nyBPL!:!y&*$ut82t,JIM";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:621;}
